48f908a6c2b8094d9c0a37f4befa147d42646019
serhatdeveloper@hotmail.com <serhatdeveloper@hotmail.com>
2021-02-13 23:56:58 +0300

    13.02.2021 API başlangıç

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loose Coupled - gevşek bağlılık, bağımlılık soyut olan kısma. 
+        //naming convention
+        //IoC Container --> Inversion of Control --> Değişimin kontrolü
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger , apilerin datasheetleri
+            //Dependency Chain --
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById (int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
+
+
+
+////return new List<Product>
+////{
+////    new Product{ProductId=1, ProductName="Elma"},
+////    new Product{ProductId=2, ProductName="Armut"},
+////};
+///
+//IProductService productService = new ProductManager(new EfProductDal());
